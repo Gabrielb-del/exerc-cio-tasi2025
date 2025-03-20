0726597316600e4a959863f3a23e56b736b1b4aa
Sistemas de Informação <si@LAB04-24.laboratorio.dom>
2025-03-19 22:12:07 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App2'
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+      <App/>
+  </React.StrictMode>
+);
+
+/*
+function Texto() {
+  var [valor, setValor] = React.useState()
+
+  function mudarValor(e)
+  {
+    setValor(e.target.value*5)
+  }
+
+  return(
+    <div>
+    <input type ="text" onChange={(e) => mudarValor(e)}/>
+    <span> { valor }</span>
+    </div>
+  );
+
+};
+*/
+
+
